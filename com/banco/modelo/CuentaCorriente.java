93aec48d44cb62d1d09ab9597a831777666fadb4
Jose <josejoestar80@gmail.com>
2026-09-18 22:57:20 -0500

    Update CuentaCorriente.java

--- com/banco/modelo/CuentaCorriente.java
+++ com/banco/modelo/CuentaCorriente.java
@@ -1,1 +1,26 @@
-}
+package com.banco.modelo;
+
+import java.time.LocalDate;
+
+/**
+ * Cuenta corriente: requiere un depósito mínimo inicial.
+ */
+public class CuentaCorriente extends Cuenta {
+
+    private double depositoMinimo;
+
+    public CuentaCorriente(String ccc, double saldoActual, double saldoMedio,
+                           LocalDate fechaApertura, Cliente titular, Sucursal sucursal,
+                           double depositoMinimo) {
+        super(ccc, saldoActual, saldoMedio, fechaApertura, titular, sucursal);
+        this.depositoMinimo = depositoMinimo;
+    }
+
+    public double getDepositoMinimo() {
+        return depositoMinimo;
+    }
+
+    public void setDepositoMinimo(double depositoMinimo) {
+        this.depositoMinimo = depositoMinimo;
+    }
+
